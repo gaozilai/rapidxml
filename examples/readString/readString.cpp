6d2aede8ce3af01d257998a60a475f8f6092dab1
gaozilai <utrust@163.com>
2020-10-16 21:54:46 +0800

    add examples

---
+++ examples/readString/readString.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "../../rapidxml.hpp"
+using namespace rapidxml;
+int main()
+{
+    char text[] = R"(<Cameras When="2018-08-26 11:28:07" Count='2'>
+    <Item module='capTest.dll' ExtTrigger='0' color='0' id='1' name='#1'>Here is the value of Item</Item>
+    <Item module='capLinar.dll' ExtTrigger='1' shutter='4000' color='1' rotate="180" mirror="N" id='2' name='#2'/>
+</Cameras>)";
+    std::cout<<"The text to be parsed is:\n\"\n"<<text<<"\n\"\n\nparse...\n--------------------------------\n";
+
+    xml_document<> doc;    // character type defaults to char
+    doc.parse<0>(text);    // 0 means default parse flags
+    xml_node<>* xnCameras = doc.first_node();
+    std::cout << "Name of first node is '" << xnCameras->name() << "'. It has attribute:\n";
+    for (xml_attribute<> *attr = xnCameras->first_attribute(); attr; attr = attr->next_attribute())
+    {
+        std::cout << "=> " << attr->name() << " = " << attr->value() << std::endl;
+    }
+
+    int iItemCnt = 0;
+    for (xml_node<> *node = xnCameras->first_node("Item"); node; node=node->next_sibling("Item"))
+    {
+        iItemCnt++;
+        std::cout << "\n - Item " << iItemCnt << "(" << node->value() << "):\n";
+        for (xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute())
+        {
+            std::cout << "   => " << attr->name() << " = " << attr->value() << std::endl;
+        }
+    }
+    
+    int iCameraCnt = atoi(xnCameras->first_attribute("Count")->value());
+    std::cout << "\nCheck the number of cameras " << (iCameraCnt == iItemCnt ? "OK!\n" : "FAILED!\n");
+}
---
+++ examples/writeString/writeString.cpp
@@ -0,0 +1,87 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include "../../rapidxml.hpp"
+#include <time.h>
+namespace rapidxml {
+    namespace internal {
+        template<class OutIt, class Ch>
+        inline OutIt print_children(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_attributes(OutIt out, const xml_node<Ch>* node, int flags);
+        template<class OutIt, class Ch>
+        inline OutIt print_data_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_cdata_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_element_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_declaration_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_comment_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_doctype_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+        template<class OutIt, class Ch>
+        inline OutIt print_pi_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+    }
+}
+#include "../../rapidxml_print.hpp"
+
+using namespace rapidxml;
+
+int main()
+{
+    char sznow[100];
+
+#if defined(_WIN32)
+    tm tmCurTime;
+    __time64_t tim = _time64(NULL);
+    _localtime64_s(&tmCurTime, &tim);
+    strftime(sznow, sizeof(sznow), "%Y-%m-%d %H:%M:%S", &tmCurTime);
+#elif defined(linux) || defined(__LYNX)
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    sprintf(sznow, "%.24s", ctime(&ts.tv_sec));
+#endif
+
+    xml_document<> doc;
+    xml_node<>* ndCameras = doc.allocate_node(node_element, "Cameras");  // Set node name to node_name
+    ndCameras->append_attribute(doc.allocate_attribute("When", sznow));
+    ndCameras->append_attribute(doc.allocate_attribute("Count", "2"));
+    doc.append_node(ndCameras);  // 插在后面
+
+    xml_node<>* ndItem = doc.allocate_node(node_element, "Item");
+    ndCameras->prepend_node(ndItem); // 从最前插入ndCameras下级，ndItem成为ndCameras的第一个子节点。
+    ndItem->append_attribute(doc.allocate_attribute("module", "capLinar.dll"));
+    ndItem->append_attribute(doc.allocate_attribute("ExtTrigger", "1"));
+    ndItem->append_attribute(doc.allocate_attribute("shutter", "4000"));
+    ndItem->append_attribute(doc.allocate_attribute("color", "1"));
+    ndItem->append_attribute(doc.allocate_attribute("rotate", "180"));
+    ndItem->append_attribute(doc.allocate_attribute("mirror", "N"));
+    ndItem->append_attribute(doc.allocate_attribute("id", "2"));
+    ndItem->append_attribute(doc.allocate_attribute("name", "#2"));
+
+    xml_node<>* ndItem2 = doc.allocate_node(node_element, "Item", "Here is the value of Item");
+    ndCameras->insert_node(ndItem, ndItem2); // 插入ndCameras下级，插在ndItem所在的位置，ndItem下移1个位置。
+    ndItem2->append_attribute(doc.allocate_attribute("module", "capTest.dll"));
+    ndItem2->append_attribute(doc.allocate_attribute("ExtTrigger", "0"));
+    ndItem2->append_attribute(doc.allocate_attribute("color", "0"));
+    ndItem2->append_attribute(doc.allocate_attribute("id", "1"));
+    ndItem2->append_attribute(doc.allocate_attribute("name", "#1"));
+
+    xml_node<> *ndGoogle = doc.allocate_node(node_element, "a", "Google");
+    doc.insert_node(0, ndGoogle); // 插入doc下级，where=0 插在最后。
+    ndGoogle->append_attribute(doc.allocate_attribute("href", "www.google.com"));
+
+    char buffer[4096];                     
+    char *end = print(buffer, doc, 0);     
+    *end = 0;                              
+
+    std::cout << doc;
+
+    xml_node<>* rot = doc.allocate_node(node_pi, "xml version='1.0' encoding='GB2312'");
+    doc.prepend_node(rot);  // 插在前面
+    std::ofstream out("config.xml");
+    out << doc;
+}
+
