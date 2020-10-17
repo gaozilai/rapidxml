b10dd727530b98e5d9c28997669047171ad27e2b
gaozilai <utrust@163.com>
2020-10-17 10:48:08 +0800

    给示例添加一些注释

--- examples/writeString/writeString.cpp
+++ examples/writeString/writeString.cpp
@@ -45,10 +45,10 @@ int main()
 #endif
 
     xml_document<> doc;
-    xml_node<>* ndCameras = doc.allocate_node(node_element, "Cameras");  // Set node name to node_name
+    xml_node<>* ndCameras = doc.allocate_node(node_element, "Cameras"); 
     ndCameras->append_attribute(doc.allocate_attribute("When", sznow));
     ndCameras->append_attribute(doc.allocate_attribute("Count", "2"));
-    doc.append_node(ndCameras);  // 插在后面
+    doc.append_node(ndCameras);  // 从最后插入doc下级，ndCameras成为doc的最后一个子节点。
 
     xml_node<>* ndItem = doc.allocate_node(node_element, "Item");
     ndCameras->prepend_node(ndItem); // 从最前插入ndCameras下级，ndItem成为ndCameras的第一个子节点。
@@ -62,7 +62,7 @@ int main()
     ndItem->append_attribute(doc.allocate_attribute("name", "#2"));
 
     xml_node<>* ndItem2 = doc.allocate_node(node_element, "Item", "Here is the value of Item");
-    ndCameras->insert_node(ndItem, ndItem2); // 插入ndCameras下级，插在ndItem所在的位置，ndItem下移1个位置。
+    ndCameras->insert_node(ndItem, ndItem2); // 从中间插入ndCameras下级，插在ndItem所在的位置，ndItem下移1个位置。
     ndItem2->append_attribute(doc.allocate_attribute("module", "capTest.dll"));
     ndItem2->append_attribute(doc.allocate_attribute("ExtTrigger", "0"));
     ndItem2->append_attribute(doc.allocate_attribute("color", "0"));
@@ -73,15 +73,24 @@ int main()
     doc.insert_node(0, ndGoogle); // 插入doc下级，where=0 插在最后。
     ndGoogle->append_attribute(doc.allocate_attribute("href", "www.google.com"));
 
+    // 输出xml文本到buffer
     char buffer[4096];                     
-    char *end = print(buffer, doc, 0);     
+    char *end = print(buffer, doc, 0); // 0 or 1=print_no_indenting
     *end = 0;                              
 
+    // xml文本打印到屏幕
     std::cout << doc;
 
+    // 在第一行添加声明
     xml_node<>* rot = doc.allocate_node(node_pi, "xml version='1.0' encoding='GB2312'");
     doc.prepend_node(rot);  // 插在前面
+
+    // xml文本写入文件config.xml (缩进并格式化文本)
     std::ofstream out("config.xml");
-    out << doc;
+    out << doc; // = print(std::ostream_iterator<char>(out), doc);
+    
+    // xml文本写入文件config_no_indenting.xml (无缩进)
+    std::ofstream out_no_indenting("config_no_indenting.xml");
+    print(std::ostream_iterator<char>(out_no_indenting), doc, print_no_indenting);
 }
 
